180dfd3a930423f00618bac3cfc659e0d5cde4fe
Prakash Vaithilingam (RBEI/BSJ22) <vap6cob@bosch.com>
2017-01-12 15:24:09 +0530

    160BY2 SMS gateway implemented

--- lib/160by2-api/160by2-new-api.php
+++ lib/160by2-api/160by2-new-api.php
@@ -38,6 +38,7 @@ class SMS160BY2NewClient
 
         curl_setopt($this->curl, CURLOPT_URL, "http://www.160by2.com/re-login");
         curl_setopt($this->curl, CURLOPT_POST, true);
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->curl, CURLOPT_POSTFIELDS, "rssData=&username=8825248400&&password=tsp231087");
         curl_setopt($this->curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.0.5) Gecko/2008120122 Firefox/3.0.5");
         curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, 60);
@@ -205,8 +206,6 @@ function sendSMS160by2($uid, $pwd, $phone, $msg)
     $client->logout();
     return $result;
 }
-
-//sendSMS160by2 ( '8825248400' , 'tsp231087' , '8825248400' , 'Hello World');
 
 ?>
 
