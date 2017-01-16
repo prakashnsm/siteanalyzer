c441bc43984ace4afeb1b9f8ec3f5a9ab2636003
Prakash Vaithilingam (RBEI/BSJ22) <vap6cob@bosch.com>
2017-01-16 13:34:25 +0530

    add new send sms testing file

---
+++ public/SendSMS.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'lib/way2sms-api.php';
+require 'lib/160by2-api/160by2-new-api.php';
+
+
+$account = $_REQUEST['acc'] ? $_REQUEST['acc'] : 0;
+
+
+
+if ( isset($_REQUEST['user']) && isset($_REQUEST['pass']) && isset($_REQUEST['mobno']) && isset($_REQUEST['msg'])) {
+	$user = $_REQUEST['user'];
+	$pass = $_REQUEST['pass'];
+	$mobNo = $_REQUEST['mobno'];
+	$message = $_REQUEST['msg'];
+		$res=0;
+		if($account == 0){
+			$res = sendWay2SMS($user, $pass, $mobNo, $message);
+		}
+	    else if($account == 1){
+			$res = sendSMS160by2($user, $pass, $mobNo, $message);
+		}
+		
+		echo '<PRE>'; var_dump($res);echo '</PRE>';
+	}
+
+?>
